590b1957a9ea932dd527829efc063da68fda1779
Keith <keithbradley1@msn.com>
2016-04-13 15:20:58 -0500

    .

--- main.js
+++ main.js
@@ -2,65 +2,85 @@
 
 // OBJECTS FOR OUR ARRAY
 
-var red-dragon = {
-	image:
-	name: 'Fierce Crimson Dragon Egg'
-	price: '§12,3456.00'
+var redDragon = {
+	image: '',
+	name: 'Fierce Crimson Dragon Egg',
+	price: '§12,3456.00',
 	description: 'The more she shat hoster tully dunk barristan jaehaerys strong belwas osha.'
 }
 
-var blue-dragon = {
-	image:
-	name: 'Wise Azure Dragon Egg'
-	price: '§12,3456.00'
+var blueDragon = {
+	image:'',
+	name: 'Wise Azure Dragon Egg',
+	price: '§12,3456.00',
 	description: 'Daenerys donal noye arianne jeyne westerling eddard maegor bronn khal drogo nymeria shireen old nan meera reed.'
 }
 
-var green-dragon = {
-	image:
-	name: 'Glorious Emerald Dragon Egg'
-	price: '§12,3456.00'
+var greenDragon = {
+	image:'',
+	name: 'Glorious Emerald Dragon Egg',
+	price: '§12,3456.00',
 	description: 'Asha elia daeron oswell whent kevan. Roose bolton varys rodrik illyrio oberyn aeron.'
 }
 
-var white-dragon = {
-	image:
-	name: 'Blue-Eyes White Dragon Egg'
-	price: '§12,3456.00'
+var whiteDragon = {
+	image:'',
+	name: 'Blue-Eyes White Dragon Egg',
+	price: '§12,3456.00',
 	description: 'Beric dondarrion dunk nymeria khal drogo. Nights king melisandre jojen gilly, jon snow chett sansa ygritte drogon lord of bones.'
 }
 
-var black-dragon = {
-	image:
-	name: 'Obsidian Demon Dragon Egg'
-	price: '§12,3456.00'
+var blackDragon = {
+	image:'',
+	name: 'Obsidian Demon Dragon Egg',
+	price: '§12,3456.00',
 	description: 'Mya stone summer ygritte, kevan patchface darkstar daenerys.'
 }
 
-var pink-dragon = {
-	image:
-	name: 'Amorous Rose Dragon Egg'
-	price: '§12,3456.00'
+var pinkDragon = {
+	image:'',
+	name: 'Amorous Rose Dragon Egg',
+	price: '§12,3456.00',
 	description: 'Eddard jaqen aerys, khal drogo stannis the hound margaery jeor mormont mya stone hoster tully quentyn brandon stark jorah.'
 }
 
-var purple-dragon = {
-	image:
-	name: 'Royal Violet Dragon Egg'
-	price: '§12,3456.00'
+var purpleDragon = {
+	image:'',
+	name: 'Royal Violet Dragon Egg',
+	price: '§12,3456.00',
 	description: 'Hoster tully samwell jon connington summer aemon cersei sansa beric dondarrion nights king viserys kevan.'
 }
 
-var orange-dragon = {
-	image:
-	name: 'Flaming Vermilion Dragon Egg'
-	price: '§12,3456.00'
+var orangeDragon = {
+	image:'',
+	name: 'Flaming Vermilion Dragon Egg',
+	price: '§12,3456.00',
 	description: 'Rhaegar aeron old nan, illyrio maester luwin aegon arthur dayne. '
 }
 
 // THE ARRAY
 
-var dragon-eggs = [ red-dragon, blue-dragon, green-dragon, white-dragon, black-dragon, pink-dragon, purple-dragon, orange-dragon ]
+var dragonEggs = [ redDragon, blueDragon, greenDragon, whiteDragon, blackDragon, pinkDragon, purpleDragon, orangeDragon ]
 
 // INSERTING THE ARRAY INTO TO THE DOM
+
+var divDragon = document.getElementById("dragon-eggs");
+
+var loopCount = dragonEggs.length
+
+for (var loopTracker = 0; loopTracker < loopCount; loopTracker += 1) {
+
+divDragon.innerHTML += 
+"<div>" + 
+"<p>" + dragonEggs[loopTracker].image + "</p>"
++ 
+"<p>" + dragonEggs[loopTracker].name + "</p>"
++
+"<p>" + dragonEggs[loopTracker].price + "</p>"
++
+"<p>" + dragonEggs[loopTracker].description + "</p>"
++
+"</div>";
+}
+
 
